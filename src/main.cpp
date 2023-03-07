4476998427471e1e787aa95af0c5f50f01c8ea05
IgorKilipenko <igorkili@mail.ru>
2023-03-07 11:23:04 +0700

    first start

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,22 @@
 #include <string>
 #include <vector>
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+
+#include "../libs/httplib.h"
 #include "domain.h"
 
 int main() {
+    {
+        using namespace httplib;
+
+        //httplib::Client cli("http://cpp-httplib-server.yhirose.repl.co");
+        httplib::Client cli("https://cpp-httplib-server.yhirose.repl.co");
+
+        auto res = cli.Get("/hi");
+        std::cout << res->status << std::endl;
+    }
+
     QueryBuilder builder{};
     try {
         std::cout << builder.getRawData() << std::endl;
