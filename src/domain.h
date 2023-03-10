a3a6f7ef06840adef730ee095df1290d2fbe2922
IgorKilipenko <igorkili@mail.ru>
2023-03-10 21:04:53 +0700

    fix getRawData

--- src/domain.h
+++ src/domain.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <optional>
 #include <string>
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "../libs/httplib.h"
 
 using namespace std::string_literals;
 
@@ -9,9 +13,33 @@ public:
     std::string getRawData() const;
 
 private:
-    const std::string gmap_req_url = "https://www.google.com/maps/@38.7848036,-97.6093175,57m/data=!3m1!1e3"s;
+    const std::string gmap_req_url_ = "https://www.google.com"s;
 };
 
 inline std::string QueryBuilder::getRawData() const {
-    return "empty"s;
+    using namespace httplib;
+
+    httplib::Client cli(gmap_req_url_);
+
+    std::optional<httplib::Result> res;
+    try {
+        res = cli.Get("/maps/@38.7848036,-97.6093175,57m/data=!3m1!1e3");
+
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        res = std::nullopt;
+    }
+
+    if (res.has_value()) {
+        try {
+            auto resp = res->value();
+            std::cout << resp.status << std::endl;
+            return res->value().body;
+
+        } catch (const std::exception& e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    return ""s;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,12 @@
 #include <string>
 #include <vector>
 
-#define CPPHTTPLIB_OPENSSL_SUPPORT
 
-#include "../libs/httplib.h"
+
+//#include "../libs/httplib.h"
 #include "domain.h"
 
 int main() {
-    {
-        using namespace httplib;
-
-        //httplib::Client cli("http://cpp-httplib-server.yhirose.repl.co");
-        httplib::Client cli("https://cpp-httplib-server.yhirose.repl.co");
-
-        auto res = cli.Get("/hi");
-        std::cout << res->status << std::endl;
-    }
-
-    QueryBuilder builder{};
-    try {
-        std::cout << builder.getRawData() << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    QueryBuilder builder;
+    std::cout << builder.getRawData() << std::endl;
 }
