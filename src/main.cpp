10cc996c89e9219a1f3f8c58f64a71c078c65c66
IgorKilipenko <igorkili@mail.ru>
2023-05-20 16:02:23 +0700

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,9 @@
 #include "domain.h"
 #include "map_version_info.h"
 
-
 int main() {
-    // maps::RawDataLoader loader;
-    // const std::string data = loader.GetRawData();
-    // maps::Parser_ parser{};
-    // std::string_view line = parser.FindVersionInfoLine(data);
-    // auto version = parser.GetVersion(line);
-
     maps::MapVersionInfo version_getter;
     const auto version = version_getter.GetMapVersion();
 
-    std::cout << std::endl <<"Google Maps version: " << version << std::endl << std::endl;
+    std::cout << std::endl << "Google Maps version: " << version << std::endl << std::endl;
 }
