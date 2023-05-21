9379287c825a617005d483c6fc61fba2a72a942f
IgorKilipenko <igorkili@mail.ru>
2023-05-21 10:27:18 +0700

    fix for libs

---
+++ src/raw_data_loader.cpp
@@ -0,0 +1,35 @@
+
+#include "raw_data_loader.h"
+
+namespace maps {
+    inline std::string RawDataLoader::GetRawData() const {
+        std::string result;
+        httplib::Client cli(gmap_base_url_);
+        std::optional<httplib::Result> response;
+
+        try {
+            response = cli.Get(gmap_query_);
+
+        } catch (const std::exception& e) {
+            std::cerr << e.what() << std::endl;
+            response = std::nullopt;
+        }
+
+        if (response.has_value()) {
+            try {
+                auto resp = response->value();
+                if (resp.status != 200) {
+                    std::cerr << "Error data loading.\nCode: " << resp.status << std::endl;
+                    return result;
+                }
+
+                result = response->value().body;
+
+            } catch (const std::exception& e) {
+                std::cerr << e.what() << std::endl;
+            }
+        }
+
+        return result;
+    }
+}
--- src/raw_data_loader.h
+++ src/raw_data_loader.h
@@ -21,38 +21,7 @@ namespace maps {
 
     private:
         // Google Maps
-        inline static const std::string gmap_base_url_ = MapProviderUrls::GoogleMap.base_url;
-        inline static const std::string gmap_query_ = MapProviderUrls::GoogleMap.query;
+        const std::string gmap_base_url_ = MapProviderUrls::GoogleMap.base_url;
+        const std::string gmap_query_ = MapProviderUrls::GoogleMap.query;
     };
-
-    inline std::string RawDataLoader::GetRawData() const {
-        std::string result;
-        httplib::Client cli(gmap_base_url_);
-        std::optional<httplib::Result> response;
-
-        try {
-            response = cli.Get(gmap_query_);
-
-        } catch (const std::exception& e) {
-            std::cerr << e.what() << std::endl;
-            response = std::nullopt;
-        }
-
-        if (response.has_value()) {
-            try {
-                auto resp = response->value();
-                if (resp.status != 200) {
-                    std::cerr << "Error data loading.\nCode: " << resp.status << std::endl;
-                    return result;
-                }
-
-                result = response->value().body;
-
-            } catch (const std::exception& e) {
-                std::cerr << e.what() << std::endl;
-            }
-        }
-
-        return result;
-    }
 }
